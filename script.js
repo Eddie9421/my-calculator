0dff4204788d5334a6a62837a01bd129d3f9f498
Eddie <Eddie11910@outlook.com>
2025-02-20 01:20:03 -0800

    Added a starter UI

--- script.js
+++ script.js
@@ -14,6 +14,20 @@ function divide(numA, numB) {
     return numA / numB;
 }
 
+function operate(leftOperand, operator, rightOperand) {
+    switch (operator) {
+        case '+':
+            return add(leftOperand, rightOperand);
+        case '-':
+            return subtract(leftOperand, rightOperand);
+        case '*':
+            return multiply(leftOperand, rightOperand);
+        case '/':
+            return divide(leftOperand, rightOperand);
+        
+    }
+}
+
 let leftOperand;
 let operator;
 let rightOperand;
