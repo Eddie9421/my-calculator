6c37bdc41ec9ea4cbc1a7fd8c0b40e06131af42d
Eddie <Eddie11910@outlook.com>
2025-02-20 01:09:12 -0800

    Created basic calculator functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(numA, numB) {
+    return numA + numB;
+}
+
+function subtract(numA, numB) {
+    return numA - numB;
+}
+
+function multiply(numA, numB) {
+    return numA * numB;
+}
+
+function divide(numA, numB) {
+    return numA / numB;
+}
+
+let leftOperand;
+let operator;
+let rightOperand;
